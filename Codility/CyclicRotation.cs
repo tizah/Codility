8445bfbf941e331a216d6772178a38f0f14f48b3
David <David@DESKTOP-VJMTNVA>
2018-10-21 06:56:56 +0100

    Cyclic Rotations

---
+++ Codility/CyclicRotation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    public class CyclicRotation
+    {
+        /// <summary>
+        /// For example, given
+
+//        A = [3, 8, 9, 7, 6]
+//        K = 3
+//the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+//    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+//        [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+//        [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int[] RotateArrayKtimes(int[] array, int k)
+        {
+            return array;
+        }
+    }
+}
