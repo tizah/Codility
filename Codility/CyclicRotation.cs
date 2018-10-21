9e74009187d180074b825460219d01eea7ce1fa3
David <David@DESKTOP-VJMTNVA>
2018-10-21 06:58:18 +0100

    few Changes to the comments

--- Codility/CyclicRotation.cs
+++ Codility/CyclicRotation.cs
@@ -11,13 +11,13 @@ namespace Codility
         /// <summary>
         /// For example, given
 
-//        A = [3, 8, 9, 7, 6]
-//        K = 3
-//the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+        //  A = [3, 8, 9, 7, 6]
+        //  K = 3
+        //the function should return [9, 7, 6, 3, 8]. Three rotations were made:
 
-//    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
-//        [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
-//        [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+        //  [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+        //  [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+        //  [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
         /// </summary>
         /// <param name="array"></param>
         /// <param name="k"></param>
